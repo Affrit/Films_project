47460aa01eb193e189a38d74783c431a6d661d03
Affrit <79639177+Affrit@users.noreply.github.com>
2021-09-17 17:39:04 +0300

    Now liked films will add to localStorage, and show on favorite page, also if films was disliked it was remove frome localStorage and favorite page. Need refactor and fix films page, need general routing

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const BASE_URL = 'https://api.tvmaze.com/'
 
+const favorite = document.getElementById('favorite') // temp
+const films = document.getElementById('films') // temp
+const main = document.getElementById('main') // temp
 const searchInput = document.getElementById('searchInput')
 const searchBtn = document.getElementById('searchBtn')
 const genreSelect = document.getElementById('genreSelect')
@@ -8,8 +11,11 @@ const filmsContainer = document.getElementById('filmsContainer')
 const countOfFoundFilms = document.getElementById('countOfFoundFilms')
 const loadMoreBtn = document.getElementById('loadMoreBtn')
 
-let commonFilmList = []
+let commonFilmList = [] // all films from server
 let filtredFilmList = []
+let filmsOnPageNow = [] // films that display on page now
+let favoriteFilmList = JSON.parse(localStorage.getItem('favoriteFilms')) // favorite list
+
 let maxCountOnPage = 14
 let wasSpawnedOnPage = 0
 
@@ -42,7 +48,7 @@ const spawnCountOfFilms = (filmList) => {
     countOfFoundFilms.innerText = `${filmList.length} films was found`
 }
 
-const spawnFilms = (filmList, maxCount) => {
+const spawnFilms = (filmList, targetBlock, maxCount = 3) => {
     // if user want to display on page more than filmList includes, user will see rest of filmList
     if (wasSpawnedOnPage + maxCount > filmList.length) {
         maxCount = filmList.length - 1 - wasSpawnedOnPage
@@ -52,6 +58,7 @@ const spawnFilms = (filmList, maxCount) => {
         const filmItem = filmList[i + wasSpawnedOnPage]
         const imgUrl = filmItem.image?.medium || '#'
         const filmName = filmItem.name || 'Name not found'
+        const filmId = filmItem.id // id from server
 
         const filmNode = createElement('div', 'class', `film`)
         const filmVisualNode = createElement('div', 'class', `film__visual`)
@@ -60,14 +67,19 @@ const spawnFilms = (filmList, maxCount) => {
         filmNameNode.innerText = filmName
         const imgNode = createElement('img', 'src', `${imgUrl}`)
         const filmLikeBtnNode = createElement('a', 'class', `film__like`)
-        filmNode.setAttribute('id', filmItem.id) // id from server
+        if (favoriteFilmList.some(film => filmItem.id === film.id)) {
+            filmLikeBtnNode.setAttribute('class', 'film__liked')
+        }
+        filmNode.setAttribute('id', filmId)
 
         filmVisualNode.append(imgNode)
         filmBodyNode.append(filmNameNode)
         filmBodyNode.append(filmLikeBtnNode)
         filmNode.append(filmVisualNode)
         filmNode.append(filmBodyNode)
-        filmsContainer.append(filmNode)
+        targetBlock.append(filmNode)
+
+        filmsOnPageNow.push(filmItem)
     }
 
     wasSpawnedOnPage += maxCount + 1
@@ -96,12 +108,11 @@ async function showContent() {
             showError('Empty :-(', filmsContainer)
             return
         }
-        commonFilmList = response
+        commonFilmList = [...response]
         filtredFilmList = [...commonFilmList]
 
-        console.log(filtredFilmList)
         spawnCountOfFilms(filtredFilmList)
-        spawnFilms(filtredFilmList, maxCountOnPage)
+        spawnFilms(filtredFilmList, filmsContainer, maxCountOnPage)
 
     } catch (error) {
         showError(error, filmsContainer)
@@ -145,23 +156,21 @@ const getfiltredFilms = (filtrationOptions, filmList) => {
 
 const onChooseGenre = ({ target }) => {
     filtrationOptions.genre = target.value
-    console.log(target.value)
 }
 
 const onChooseLang = ({ target }) => {
     filtrationOptions.lang = target.value
-    console.log(target.value)
 }
 
 const onSearch = () => {
     clearContainer(filmsContainer)
+    filmsOnPageNow = []
     wasSpawnedOnPage = 0
     filtrationOptions.searchWord = searchInput.value
 
     filtredFilmList = getfiltredFilms(filtrationOptions, commonFilmList)
     spawnCountOfFilms(filtredFilmList)
-    spawnFilms(filtredFilmList, maxCountOnPage)
-    console.log(filtrationOptions)
+    spawnFilms(filtredFilmList, filmsContainer, maxCountOnPage)
 }
 
 const onEnter = (e) => {
@@ -170,13 +179,83 @@ const onEnter = (e) => {
     }
 }
 
+const onFilmLikeBtnPushed = (event) => {
+    if (event.target.nodeName === 'A') {
+        const filmId = +event.path[2].id
+        let newFavoriteFilmList = [...favoriteFilmList]
+        const idInArray = favoriteFilmList.findIndex(film => film.id === filmId) // есть ли уже
+        if (idInArray !== -1) {
+            newFavoriteFilmList.splice(idInArray, 1)
+        } else {
+            const likedFilm = filmsOnPageNow.find(film => film.id === filmId)
+            newFavoriteFilmList = [...favoriteFilmList, likedFilm]
+        }
+        const newFavoriteFilmListJson = JSON.stringify(newFavoriteFilmList)
+        localStorage.setItem('favoriteFilms', newFavoriteFilmListJson)
+        favoriteFilmList = newFavoriteFilmList
+
+        if (event.target.className === 'film__like') {
+            event.target.setAttribute('class', 'film__liked')
+        } else {
+            event.target.setAttribute('class', 'film__like')
+        }
+    }
+}
+
 searchInput.addEventListener('keydown', onEnter)
 searchBtn.addEventListener('click', onSearch)
 genreSelect.addEventListener('change', onChooseGenre)
 langSelect.addEventListener('change', onChooseLang)
+filmsContainer.addEventListener('click', onFilmLikeBtnPushed)
 
 const onLoadMore = () => {
-    spawnFilms(filtredFilmList, maxCountOnPage)
+    spawnFilms(filtredFilmList, filmsContainer, maxCountOnPage)
 }
 
 loadMoreBtn.addEventListener('click', onLoadMore)
+
+
+
+
+
+
+
+
+// ======== render Search =============
+const renderSearch = () => {
+    // 
+}
+
+// ======== render Films ==============
+const renderFilms = () => {
+    // renderHeader
+    // renderSearch
+    // renderCountOfFilms 
+
+    clearContainer(main) // temp
+    wasSpawnedOnPage = 0
+    filmsOnPageNow = []
+
+    const filmsContainer = createElement('div', 'class', `films`)
+    filmsContainer.setAttribute('id', 'filmsContainer')
+    spawnFilms(filtredFilmList, filmsContainer, maxCountOnPage)
+    main.append(filmsContainer)
+}
+
+films.addEventListener('click', renderFilms)
+
+// ======== render Favorites ==========
+const renderFavorites = () => {
+    clearContainer(main) // temp
+    wasSpawnedOnPage = 0
+    filmsOnPageNow = []
+
+    const filmsContainer = createElement('div', 'class', `films`)
+    filmsContainer.setAttribute('id', 'filmsContainer')
+    spawnFilms(favoriteFilmList, filmsContainer, maxCountOnPage)
+    main.append(filmsContainer)
+
+    filmsContainer.addEventListener('click', onFilmLikeBtnPushed)
+}
+
+favorite.addEventListener('click', renderFavorites)
