3aa801c674ed698e809c578ba8ef547ce86daa77
Affrit <79639177+Affrit@users.noreply.github.com>
2021-10-05 23:20:54 +0300

    single responsibility for spawnilms

--- js/utils/spawnFilms.js
+++ js/utils/spawnFilms.js
@@ -1,33 +1,44 @@
 import { variables } from "../variables.js"
 import { createElement } from "./utils.js"
 
+const createFilmNode = (filmOptions) => {
+    const { filmId, filmName, filmImgUrl } = filmOptions
+
+    const filmNode = createElement('div', 'class', 'film')
+    const filmVisualNode = createElement('div', 'class', 'film__visual')
+    const filmBodyNode = createElement('div', 'class', 'film__body')
+    const filmNameNode = createElement('span', 'class', 'film__name')
+    filmNameNode.innerText = filmName
+    const imgNode = createElement('img', 'src', `${filmImgUrl}`)
+    const filmLikeBtnNode = createElement('a', 'class', 'film__like')
+
+    if (variables.favoriteFilmList.some(film => film.id === filmId)) {
+        filmLikeBtnNode.setAttribute('class', 'film__liked')
+    }
+
+    filmNode.setAttribute('id', filmId)
+
+    filmVisualNode.append(imgNode)
+    filmBodyNode.append(filmNameNode)
+    filmBodyNode.append(filmLikeBtnNode)
+    filmNode.append(filmVisualNode)
+    filmNode.append(filmBodyNode)
+
+    return filmNode
+}
+
 export const spawnFilms = (filmList, targetBlock, maxCount) => {
-    
     for (let i = 0; i < maxCount; i++) {
-        const filmItem = filmList[i + variables.wasSpawnedOnPage]
-        if (!filmItem) break // if want to display more films than filmList includes
-        
-        const imgUrl = filmItem.image?.medium || '../img/NoImage.png'
-        const filmName = filmItem.name || 'Name not found'
-        const filmId = filmItem.id // id from server
+        const filmItem = filmList[i + variables.wasSpawnedOnPage] // for show more
+        if (!filmItem) break // if user want to display more films than filmList includes
 
-        const filmNode = createElement('div', 'class', `film`)
-        const filmVisualNode = createElement('div', 'class', `film__visual`)
-        const filmBodyNode = createElement('div', 'class', `film__body`)
-        const filmNameNode = createElement('span', 'class', `film__name`)
-        filmNameNode.innerText = filmName
-        const imgNode = createElement('img', 'src', `${imgUrl}`)
-        const filmLikeBtnNode = createElement('a', 'class', `film__like`)
-        if (variables.favoriteFilmList.some(film => filmItem.id === film.id)) {
-            filmLikeBtnNode.setAttribute('class', 'film__liked')
+        const filmOptions = {
+            filmImgUrl: filmItem.image?.medium || '../img/NoImage.png',
+            filmName: filmItem.name || 'Name not found',
+            filmId: filmItem.id, // id from server
         }
-        filmNode.setAttribute('id', filmId)
 
-        filmVisualNode.append(imgNode)
-        filmBodyNode.append(filmNameNode)
-        filmBodyNode.append(filmLikeBtnNode)
-        filmNode.append(filmVisualNode)
-        filmNode.append(filmBodyNode)
+        const filmNode = createFilmNode(filmOptions)
         targetBlock.append(filmNode)
 
         variables.filmsOnPageNow.push(filmItem)
